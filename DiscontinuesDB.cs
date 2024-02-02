115df930af5e3492c9df99f9263f3fed85550936
Belginbalakrishanan <belginbalakrishnan@gmail.com>
2024-02-02 20:58:50 +0530

    Add project files.

---
+++ DiscontinuesDB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace PracticeADO
+{
+    internal class DiscontinuesDB
+    {
+        public static void SelectType()
+        {
+            string constr=ConfigurationManager.ConnectionStrings["cn"].ToString();
+            SqlConnection con = new SqlConnection(constr);
+            string query = "select * from employee";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(query,con);
+            DataSet dataset = new DataSet();
+            dataAdapter.Fill(dataset);
+            DataTable tables = dataset.Tables[0];
+            foreach(DataRow row in tables.Rows)
+            {
+                Console.WriteLine(row[0] + "\t" + row[1] + "\t" + row[2]);
+            }
+        }
+    }
+}
---
+++ ParameterQuery.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeADO
+{
+    internal class ParameterQuery
+    {
+        public static void ParameterQueries()
+        {
+            string constr=ConfigurationManager.ConnectionStrings["cn"].ToString();
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            string query = "insert into employee values(@empid,@ename,@deptno)";
+            SqlCommand cmd = new SqlCommand(query, con);
+
+           
+            SqlParameter idpm = new SqlParameter("@empid",SqlDbType.Int);
+            idpm.Direction = ParameterDirection.Input;
+            Console.WriteLine("Enter id");
+            idpm.Value=int.Parse(Console.ReadLine());
+            cmd.Parameters.Add(idpm);
+
+            SqlParameter namepm = new SqlParameter("@ename", SqlDbType.VarChar,20);
+            namepm.Direction = ParameterDirection.Input;
+            Console.WriteLine("Enter name");
+            namepm.Value =Console.ReadLine();
+            cmd.Parameters.Add(namepm);
+
+            SqlParameter deptpm = new SqlParameter("@deptno", SqlDbType.Int);
+            deptpm.Direction = ParameterDirection.Input;
+            Console.WriteLine("Enter deptno");
+            deptpm.Value = int.Parse(Console.ReadLine());
+            cmd.Parameters.Add(deptpm);
+
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
